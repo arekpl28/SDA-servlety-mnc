db8b2b836b2fd45cc8457c00a53b2923480b92a6
unknown <arekpl28@wp.pl>
2017-03-07 21:02:02 +0100

    Added mehtods doPost

--- src/main/java/com/arek/servlet/HelloServlet.java
+++ src/main/java/com/arek/servlet/HelloServlet.java
@@ -5,11 +5,15 @@ import com.arek.controller.CookieController;
 import com.arek.controller.LoginController;
 import com.arek.controller.UserController;
 import org.apache.commons.lang3.StringUtils;
+import pl.sda.file.FileOperation;
+import pl.sda.file.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Calendar;
@@ -43,6 +47,23 @@ public class HelloServlet extends HttpServlet {
 
 //        PrintWriter writer = resp.getWriter();
 //        writer.write("<h1>404 error: File not found \n</h1><h3>The URL requested was not found</h3>");
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BufferedReader reader = req.getReader();
+        String body = reader.readLine();
+        String[] split = body.split(" ");
+
+        User user = new User();
+        user.setFirstName(split[0]);
+        user.setLastName(split[1]);
+        user.setAge(new Integer(split[2]));
+
+        File file = new File("C:\\Users\\RENT\\Desktop\\test.txt");
+        FileOperation.addUserToFile(user, file);
+
+        resp.getWriter().write("Wszystko ok");
+        resp.setStatus(201);
     }
 }
